253d451fc9b91c13462017d9c86d42ddf150fb9a
glatard <glatard@users.noreply.github.com>
2022-10-08 00:12:18 +0000

    Generated utility functions for the notebooks.

--- livingpark_utils/scripts/run.py
+++ livingpark_utils/scripts/run.py
@@ -8,13 +8,13 @@ import importlib
 from IPython.utils import io
 
 
-def mri_metadata():
-    """Execute auto-generated script for `../notebooks/mri_metadata.ipynb`."""
-    with io.capture_output():
-        importlib.import_module(f"livingpark_utils.scripts.mri_metadata")
-
-
 def pd_status():
     """Execute auto-generated script for `../notebooks/pd_status.ipynb`."""
     with io.capture_output():
         importlib.import_module(f"livingpark_utils.scripts.pd_status")
+
+
+def mri_metadata():
+    """Execute auto-generated script for `../notebooks/mri_metadata.ipynb`."""
+    with io.capture_output():
+        importlib.import_module(f"livingpark_utils.scripts.mri_metadata")
