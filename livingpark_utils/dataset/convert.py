06b1432fbee7a63722e37dbd492a8cf662167a44
mathdugre <mathdugre@pm.me>
2023-06-15 12:57:08 -0400

    [FIX] linting.

--- livingpark_utils/dataset/convert.py
+++ livingpark_utils/dataset/convert.py
@@ -1,14 +1,36 @@
-import logging
+"""Utility functions to convert datasets between different file formats."""
 from pathlib import Path
+from typing import Any
 
 from nipype.interfaces.dcm2nii import Dcm2niix
 
 
 class fileConversionError(Exception):
+    """Exception to report a failed file conversion."""
+
     pass
 
 
-def dcm2niix(filenames: list[Path] | Path, output_dir: Path = None):
+def dcm2niix(filenames: list[Path] | Path, output_dir: Path = None) -> Any:
+    """Convert DICOM files to NIfTI using dcm2niix.
+
+    Parameters
+    ----------
+    filenames : list[Path] | Path
+        File to convert. Can either be a list of files or a directory.
+    output_dir : Path, optional
+        Path to the output folder, by default None
+
+    Returns
+    -------
+    Any
+        If conversion is successful, returns the converter. Otherwise, None.
+
+    Raises
+    ------
+    fileConversionError
+        When a file conversion fails.
+    """
     converter = Dcm2niix()
     if isinstance(filenames, Path) and filenames.is_dir():
         converter.inputs.source_dir = filenames
@@ -21,5 +43,5 @@ def dcm2niix(filenames: list[Path] | Path, output_dir: Path = None):
 
     try:
         return converter.run()
-    except:
-        raise fileConversionError
+    except Exception as e:
+        raise fileConversionError(e)
